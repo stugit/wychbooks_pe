eca2ccac616504e4e40433fa0f0da1aefcb4050e
Karwoo Tang <karwoo.tang@gmail.com>
2016-10-13 23:46:52 +0100

    add udt.sh

---
+++ udt.sh
@@ -0,0 +1,6 @@
+# This file belongs in /etc/profile.d/udt.sh
+export UDTBIN=/usr/ud81/bin
+export UDTHOME=/usr/ud81
+export PATH=$PATH:$UDTBIN
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$UDTBIN
+export LANG=C
